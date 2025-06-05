1bfa3932233ebc40712ba51096c1611956697798
ZeRp-LLa <mushyk.illia.s@gmail.com>
2025-06-05 15:34:22 +0300

    lab9

---
+++ lab9/src/drit.py
@@ -0,0 +1,24 @@
+def max_wire_length(w, heights):
+    n = len(heights)
+    dp = [[0.0, 0.0] for _ in range(n)]
+
+    for i in range(1, n):
+        dist_00 = w
+        dist_01 = (w**2 + (heights[i] - 1)**2)**0.5
+        dist_10 = (w**2 + (1 - heights[i-1])**2)**0.5
+        dist_11 = (w**2 + (heights[i] - heights[i-1])**2)**0.5
+
+
+        dp[i][0] = max(
+            dp[i-1][0] + dist_00,
+            dp[i-1][1] + dist_10
+        )
+
+        dp[i][1] = max(
+            dp[i-1][0] + dist_01,
+            dp[i-1][1] + dist_11
+        )
+
+    return round(max(dp[n-1][0], dp[n-1][1]), 2)
+
+print(max_wire_length())
---
+++ lab9/test/test.py
@@ -0,0 +1,36 @@
+from lab2_ap.lab9.src.drit import max_wire_length
+import unittest
+
+class TestMaxWireLength(unittest.TestCase):
+    def test_example1(self):
+        w = 2
+        heights = [3, 3, 3]
+        expected = 5.66
+        result = max_wire_length(w, heights)
+        self.assertAlmostEqual(result, expected, places=2)
+
+    def test_example2(self):
+        w = 100
+        heights = [1, 1, 1, 1]
+        expected = 300
+        result = max_wire_length(w, heights)
+        self.assertAlmostEqual(result, expected, places=2)
+
+    def test_example3(self):
+        w = 4
+        heights = [100, 2, 100, 2, 100]
+        expected = 396.32
+        result = max_wire_length(w, heights)
+        self.assertAlmostEqual(result, expected, places=2)
+
+    def test_example4(self):
+        w = 4
+        heights = [56, 18, 17, 94, 23, 7, 21, 94, 29, 54, 44, 26, 86, 79, 4, 15, 5, 91, 25, 17, 88, 
+                   66, 28, 2, 95, 97, 60, 93, 40, 70, 75, 48, 38, 51, 34, 52, 87, 8, 62, 77, 35, 52, 
+                   3, 93, 34, 57, 51, 11, 39, 72]
+        expected = 2738.18
+        result = max_wire_length(w, heights)
+        self.assertAlmostEqual(result, expected, places=2)
+
+if __name__ == '__main__':
+    unittest.main()
